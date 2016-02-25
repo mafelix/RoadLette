a77c957ff522f129b9e690197751a6ae58c5ff6d
Andy Kwan <zelthrox@gmail.com>
2016-02-25 20:42:39 +0000

    merge fix

--- app/actions.rb
+++ app/actions.rb
@@ -21,6 +21,6 @@ get '/results' do
   page = HTTParty.get('http://www.gasbuddy.com/')
   @parse_page = Nokogiri::HTML(page)
   @parse_page = @parse_page.css('.gb-price-lg')[0].children[0].to_s.to_f
-  binding.pry
+
   erb :'results/index'
 end
