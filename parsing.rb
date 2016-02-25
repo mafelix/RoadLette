00d87399ef5c8a0046e7df494dda892416c4038c
mafelix <felixmdev@gmail.com>
2016-02-25 04:37:11 +0000

    parsing edited

--- parsing.rb
+++ parsing.rb
@@ -5,7 +5,9 @@ require 'pry'
 @request = Net::HTTP.get(URI.parse('https://maps.googleapis.com/maps/api/directions/json?origin=Vancouver,+BC&destination=RIchmond,+BC&departure_time=1343641500&mode=transit&key=AIzaSyAPV0_sCF_Qe5jsKsHd5DCfVC1c3yI3MLc'))
 
 
+# response = JSON.parse(@request)["routes"][0]["legs"][0]["distance"]
 response = JSON.parse(@request)["routes"][0]["legs"][0]["distance"]
+
 
 p response
 
