b414b148554c144373f9a18e5cd5cd024b8e0353
Andrew Woo <ahjwoo@gmail.com>
2016-02-26 00:12:09 +0000

    Made signin and signout functional, changed styling a little bit

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,13 @@
 require 'pry'
+enable :sessions
 # Homepage (Root path)
+
+# helpers do
+#   def current_user
+#     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+#   end
+# end
+
 get '/' do
   erb :index
 end
@@ -17,9 +25,38 @@ get '/results/index' do
   erb :'/results/index'
 end
 
+get '/users/signup' do
+  erb :'users/signup'
+end
 
-#What we need to implement:
-# Once given a budget value, need to see how far
-# user can drive based on average gas prices..
+post '/users/signup' do
+  @user = User.new(
+    name: params[:name],
+    password: params[:password]
+    )
+  if @user.save
+    redirect '/'
+  else
+    erb :'users/signup'
+  end
+end
+
+get '/users/signin' do
+  erb :'users/signin'
+end
+
+# get '/users/signin' do
+#   @user = User.find_by(
+#     name: params[:name],
+#     password: params[:password]
+#     )
+#   if @user 
+#     session[:user_id] = @user.id
+#     redirect '/'
+#   else
+#     erb :'/users/signin'
+#   end
+# end
+
 
 
