62f4ef9eac67c66a4f873de42e2df324ffc4fe26
Andy Kwan <zelthrox@gmail.com>
2016-02-27 03:52:20 +0000

    wiki_pic

--- app/actions.rb
+++ app/actions.rb
@@ -31,16 +31,14 @@ helpers do
     lon2 = lon1 + Math.atan2(Math.sin(brng)*Math.sin(d/@r)*Math.cos(lat1), 
      Math.cos(d/@r)-Math.sin(lat1)*Math.sin(lat2))
     # => 0.0497295729068199      
-    @lat1 = 49.2820150
-    @long1 = -123.1082410
-    lat2 = degree(lat2)
-    lon2 = degree(lon2)
-    @destination_array << lat2
-    @destination_array << lon2
+    @end_lat = degree(lat2)
+    @end_long = degree(lon2)
+    @destination_array << @end_lat
+    @destination_array << @end_long
+  end
 
-    @end_lat = @destination_array[0]
-    @end_long = @destination_array[1]
-    
+
+  def city_name (coordinates)
     #geonames get request to find nearby city
     @uri = URI.parse("http://api.geonames.org/findNearbyPlaceNameJSON?lat=#{@end_lat}&lng=#{@end_long}&cities=cities1000&username=powerup7")
     @geonames = Net::HTTP.get(@uri)
@@ -48,6 +46,18 @@ helpers do
       calculate_destination
     else
       @cityname = get_city_name(JSON.parse(@geonames))
+    end
+  end
+
+  def get_wiki_link (coordinates)
+    @wiki_link = URI.parse("http://api.geonames.org/findNearbyWikipediaJSON?lat=#{@end_lat}&lng=#{@end_long}&username=powerup7")
+    geowiki = Net::HTTP.get(@wiki_link)
+    if geowiki == "{\"geonames\":[]}"
+      binding.pry
+      puts geowiki
+      nil
+    else
+      gif_link = JSON.parse(geowiki)["geonames"][0]["wikipediaUrl"]
     end
   end
 
@@ -68,10 +78,32 @@ helpers do
   end
 
   def wiki_picture (wiki_link)
-    http_page = HTTParty.get (wiki_link)
-    nokogiri_page = Nokogiri::HTML(http_page)
-    wiki_image = nokogiri_page.css('div#mw-content-text table tr')[3].css('a')[0]['href']
-    image_link = "https://en.wikipedia.org#{wiki_pic}"
+    wiki_page = wiki_link
+    binding.pry
+    
+    page = HTTParty.get (wiki_page)
+       binding.pry
+    parse_page = Nokogiri::HTML(page)
+
+    wiki_pic = []
+    (0..4).each do |i|
+      test = parse_page.css('div#mw-content-text table tr')[i].css('a')
+      (wiki_pic << test[0]['href']) unless test.empty?
+    end
+
+    pic_link = []
+    wiki_pic.each do |link|
+      pic_link << "https://en.wikipedia.org#{link}"
+    end
+
+    binding.pry
+    real_image = []
+    pic_link.each do |i|
+      a_image = Nokogiri::HTML(HTTParty.get(i)).css('.fullImageLink a')[0]
+      (real_image << "https:#{a_image['href']}") unless a_image.nil?
+    end
+
+    real_image[0]
   end
 
 end
@@ -97,13 +129,11 @@ end
 get '/results/index' do
   @travel_distance = params[:travel_distance].to_f
   calculate_destination
-
-  @end_lat = @destination_array[0]
-  @end_long = @destination_array[1]
-
+  city_name(@destination_array)
+  @wiki_link = get_wiki_link(@destination_array)
   #getting wikipedia picture
-  # wiki_picture(wiki_link)
-
+  wiki_picture(@wiki_link) unless @wiki_link.nil?
+  binding.pry
   erb :'/results/index'
 end
 
